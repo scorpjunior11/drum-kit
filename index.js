0179492c79af166db542b9c980a7788f2652aa6e
bitson <lawmauli@gmail.com>
2026-05-16 09:04:34 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+document.getElementById("w").onclick= function(){
+    let sound= new Audio("sounds/tom-1.mp3")
+    sound.play();
+}
+
+document.getElementById("a").onclick= function(){
+    let sound= new Audio("sounds/tom-2.mp3")
+    sound.play();
+}
+
+document.getElementById("s").onclick= function(){
+    let sound= new Audio("sounds/tom-3.mp3")
+    sound.play();
+}
+
+document.getElementById("d").onclick= function(){
+    let sound= new Audio("sounds/tom-4.mp3")
+    sound.play();
+}
+
+document.getElementById("j").onclick= function(){
+    let sound= new Audio("sounds/snare.mp3")
+    sound.play();
+}
+
+document.getElementById("k").onclick= function(){
+    let sound= new Audio("sounds/kick-bass.mp3")
+    sound.play();
+}
+
+document.getElementById("l").onclick= function(){
+    let sound= new Audio("sounds/crash.mp3")
+    sound.play();
+}
